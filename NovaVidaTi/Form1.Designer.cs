e6ac1907554ebaa610f5625992a5ea1fa5392583
HenriqueGreghi <henriquevsg@gmail.com>
2022-01-26 08:38:05 -0300

    Primeira tentativa de crawler

---
+++ NovaVidaTi/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace NovaVidaTi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Produto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Valor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Comentários = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Produto,
+            this.Valor,
+            this.Comentários});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 34);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(382, 351);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Produto
+            // 
+            this.Produto.HeaderText = "Produto";
+            this.Produto.Name = "Produto";
+            // 
+            // Valor
+            // 
+            this.Valor.HeaderText = "Valor";
+            this.Valor.Name = "Valor";
+            // 
+            // Comentários
+            // 
+            this.Comentários.HeaderText = "Comentários";
+            this.Comentários.Name = "Comentários";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(460, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(301, 351);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Produto;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Valor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Comentários;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ NovaVidaTi/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+
+namespace NovaVidaTi
+{
+    public partial class Form1 : Form
+    {
+
+        string Link = "https://www.kabum.com.br/lancamentos";
+        string Xpath = "/html/body/div[1]/main/article/section/div[2]/div/main";
+        //              /html/body/div[1]/main/article/section/div[2]/div/main/div[position()>0]
+        //              /html/body/div[1]/main/article/section/div[2]/div/main/div[1]/a/div/div[1]/h2
+        //              /html/body/div[1]/main/article/section/div[2]/div/main/div[1]/a/div[2]/div[2]/div[1]/div/span[2]
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            LoadProduct();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        void LoadProduct()
+        {
+            var Products = new List<Product>();
+
+            var Web = new HtmlWeb();
+
+            var Dock = Web.Load(Link);
+
+            var Nodes = Dock.DocumentNode.SelectNodes(Xpath);
+
+            foreach(var Node in Nodes)
+            {
+                try
+                {
+                    var Product = new Product
+                    {
+                        Name = Node.SelectSingleNode("div[1]/a/div/div[1]/h2").InnerText,
+                        Comment = Node.SelectSingleNode("div[1]/a/div[2]/div[2]/div[1]/div/span[2]").InnerText
+                    };
+
+                    Products.Add(Product);
+                }
+                catch { }
+                
+
+            }
+
+            foreach (var Product in Products)
+            {
+                dataGridView1.Rows.Add(Product.Name, Product.Comment);
+            }
+        }
+
+        class Product
+        {
+            public string Name { get; set; }
+
+            public string Comment { get; set; }
+
+            internal void Add(Product product)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+
+}
